14e37ae3c32e83459b7c4f49b7358bc98f6ff1e0
George Medhat <92307957+GeorgeMedhat@users.noreply.github.com>
2024-07-15 18:44:53 +0300

    FINISHED WEBSITE eddited on small devices

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -14,7 +14,7 @@ const Hero =props =>{
         gap-10 max-container ">
         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
             <p className="text-xl font-montserrat text-[#ff5040]">Our Summer Collection</p>
-            <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82]">
+            <h1 className="mt-10 font-palanquin text-8xl sm:text-[72px] ">
                 <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
                 <br/>
                 <span className="text-[#ff5040] inline-block mt-3">
